5a51c4e94f261969844c2ccb6b0b50cce3526887
SilveryPaw <sir.sinitzin2012@yandex.ru>
2023-05-01 17:26:57 +0300

    Fixed links

--- ui/func/session/set_session.php
+++ ui/func/session/set_session.php
@@ -1,11 +1,11 @@
 <?php
 include_once __DIR__ . "/../../configuration/config.php";
 
-$location = $GLOBALS["absolutePath"];
+$location = $GLOBALS["absolutePath"] . "pages/search_page.php";
 session_start();
 $_SESSION["login"] = $_GET["login"];
 echo($_SESSION["login"]);
 session_write_close();
-header("Location: $absolutePath/pages/search_page.php");
+header("Location: $location");
 exit();
 ?>
